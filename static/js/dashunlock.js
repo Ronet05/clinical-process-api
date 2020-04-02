4bb5e7b10c2e52cfb99739677f075018fa538f95
Ronet05 <ronet_swaminathan@ymail.com>
2020-04-02 19:11:30 +0530

    Added jquery, not working

---
+++ static/js/dashunlock.js
@@ -0,0 +1,14 @@
+$("#optybtn").click(function(){
+        $("#more-options").toggle();
+        //$('#more-options').attr('required', '');
+        //$('#more-options').attr('data-error', 'This field is requireed');
+
+});
+
+$("#optnbtn").click(function(){
+    $('#more-options').hide();
+    //$('#more-options').removeAttr('required');
+    //$('#more-options').removeAttr('data-error');
+});
+
+//$("#optnbtn").trigger("click")
