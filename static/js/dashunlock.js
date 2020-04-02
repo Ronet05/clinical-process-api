7896145f461526f64aa6fad12e7d776aa5d7dca2
Ronet05 <ronet_swaminathan@ymail.com>
2020-04-02 20:05:58 +0530

    Added nocust button functions

--- static/js/dashunlock.js
+++ static/js/dashunlock.js
@@ -1,14 +1,11 @@
-$("#optybtn").click(function(){
-        $("#more-options").toggle();
-        //$('#more-options').attr('required', '');
-        //$('#more-options').attr('data-error', 'This field is requireed');
+$("#optybtn").click(function () {
+    $("#more-options").show();
+    $("#submit-btn").hide();
+});
+
+$("#optnbtn").click(function () {
+    $("#submit-btn").show();
+    $("#more-options").hide();
 
 });
 
-//$("#optnbtn").click(function(){
-  //  $('#more-options').hide();
-    //$('#more-options').removeAttr('required');
-    //$('#more-options').removeAttr('data-error');
-//});
-
-//$("#optnbtn").trigger("click")
