dd849163b210ee30dd21efacf41c1ed7dc89a0a4
Ronet05 <ronet_swaminathan@ymail.com>
2020-04-04 22:19:31 +0530

    dynamic selects, need to stop after 1

--- static/js/dashunlock.js
+++ static/js/dashunlock.js
@@ -1,4 +1,4 @@
-function getSelectedOptions(sel, fn){
+function getSelectedOptions(sel){
     var opts = [], opt;
     for (var i =0, len = sel.options.length; i<len; i++){
         opt = sel.options[i];
@@ -6,29 +6,67 @@ function getSelectedOptions(sel, fn){
         if(opt.selected){
             opts.push(opt);
 
-            if(fn){
+            /**if(fn){
                 fn(opt);
-            }
+            }*/
         }
     }
 
     return opts;
 }
 
-function callback(opt){
+/**function callback(opt){
     var display = document.getElementById("feature-select-div");
+    var select = document.createElement('select');
+    var option = opt;
+
+    
+    
     display.innerHTML +=opt.value+', ';
+}*/
+
+function createSelect(option_list, select){
+    var option;
+    for (var i =0; i<option_list.length; i++){
+        option = document.createElement('option');
+        option.value = option_list[i];
+        option.text = option_list[i];
+        select.appendChild(option);
+    }
 }
+
 
 document.getElementById("paramcollopt").onchange = function(e){
     var display = document.getElementById("feature-select-div");
-    display.innerHTML = '';
-
-    getSelectedOptions(this, callback);
+    display.innerHTML += "<h4> Test h4 element</h4>";
+    var labs_options = ['HCO3', 'FiO2', 'pH', 'AST', 'etc'];
+    var vitals_options = ['HR', 'O2Sat', 'Temp', 'SBP'];
+    var temp_list = [labs_options,vitals_options];
     
-    var str = display.innerHTML.slice(0,-2);
-    display.innerHTML = str;
+    options = getSelectedOptions(this);
+    
+    for (var i=0; i <options.length;i++){
+
+        var sel = document.createElement('select');
+        //select.setAttribute("multiple");
+        //var option;
+        if(i==0){
+            createSelect(labs_options, sel);
+        }
+        else{
+            createSelect(vitals_options, sel);
+        }
+        display.appendChild(sel);
+        
+        
+    }
+    
+    //var str = display.innerHTML.slice(0,-2);
+    //display.innerHTML = str;
 }
+
+
+
 
 $("#optybtn").click(function () {
     $("#feature-select-div").show();
