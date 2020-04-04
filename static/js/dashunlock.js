635e3a47956d1814a50e629514d0362780492a17
Ronet05 <ronet_swaminathan@ymail.com>
2020-04-04 12:31:23 +0530

    transfer select options

--- static/js/dashunlock.js
+++ static/js/dashunlock.js
@@ -1,11 +1,49 @@
+function getSelectedOptions(sel, fn){
+    var opts = [], opt;
+    for (var i =0, len = sel.options.length; i<len; i++){
+        opt = sel.options[i];
+
+        if(opt.selected){
+            opts.push(opt);
+
+            if(fn){
+                fn(opt);
+            }
+        }
+    }
+
+    return opts;
+}
+
+function callback(opt){
+    var display = document.getElementById("feature-select-div");
+    display.innerHTML +=opt.value+', ';
+}
+
+document.getElementById("paramcollopt").onchange = function(e){
+    var display = document.getElementById("feature-select-div");
+    display.innerHTML = '';
+
+    getSelectedOptions(this, callback);
+    
+    var str = display.innerHTML.slice(0,-2);
+    display.innerHTML = str;
+}
+
 $("#optybtn").click(function () {
-    $("#more-options").show();
+    $("#feature-select-div").show();
+    
     $("#submit-btn").hide();
 });
 
+
+
 $("#optnbtn").click(function () {
     $("#submit-btn").show();
+    $("#feature-select-div").hide();
     $("#more-options").hide();
 
 });
+
+
 
